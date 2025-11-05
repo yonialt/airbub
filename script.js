baed0332af1b2059f3b533b07e2778ab69e76818
yonialt <yonathanaltayecama@gmail.com>
2025-11-06 02:20:30 +0300

    update verstion

---
+++ script.js
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="image/favicon-96x96.png" sizes="96x96" />
+    <script src="https://kit.fontawesome.com/5b2cf778e7.js" crossorigin="anonymous"></script>
+    <title>kiraybet</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <div class="nav_bar">
+            <nav class="continer">
+                <div class="main-logo">
+                  <img src="image/favicon-96x96.png" > 
+                </div>
+                <div class="continer-midle">
+                <div class="anywher">
+                    <p class="anywhere">anywhere</p>
+                </div>
+                <div class="continer_pipe">
+                    <div class="pipe"></div>
+                </div>
+                <div class="anytime">
+                    <p class="anytime">anytime</p>
+                </div>
+                <div class="continer_pipe">
+                    <div class="pipe"></div>
+                </div>
+                <div class="anyplace">
+                    <p class="anytime">anytime</p>
+                </div>
+                <div class="icon">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </div>
+                </div>
+            
+                    <div class="becomehoset">
+                        <p class="becomehoest">become host</p>
+                    </div>
+                   <div class="continer-icon">
+
+                   </div> 
+                
+            </nav>
+        </div>
+    </header>
+</body>
+</html
